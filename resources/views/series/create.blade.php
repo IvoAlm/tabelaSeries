493609e9b8c59bce84090c9644d862dfae2c5d57
Ivo <ivodsalmeida@gmail.com>
2019-09-22 17:57:31 -0300

    Add security resource for POST request

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -4,6 +4,7 @@
 @endsection
 @section('conteudo')
     <form method="post">
+        @csrf
         <div class="form-group">
             <label for="nome">Nome</label>
             <input type="text" class="form-control" name="nome">
