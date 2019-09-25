cec63bbe368f7259c2ee28d519b9506c4f2f3034
Ivo <ivodsalmeida@gmail.com>
2019-09-25 19:43:01 -0300

    code refactoring

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -12,11 +12,13 @@ Séries
     <a href="{{ route('criar_serie') }}" class="btn btn-primary btn-lg mb-2">Adicionar</a>
     <ul class="list-group">
         <?php foreach ($series as $serie): ?>
-            <li class="list-group-item">{{$serie->nome}}
+            <li class="list-group-item d-flex justify-content-between align-items-center">{{$serie->nome}}
                 <form action="/series/{{ $serie->id }}" method="post" onclick="confirm('Tem certeza que quer apagar?')">
                     @csrf
                     @method('DELETE')
-                    <button  class="btn btn-danger">Excluir</button>
+                    <button  class="btn btn-danger">
+                        <i class="fas fa-trash-alt"></i>
+                    </button>
                 </form>
             </li>
 
